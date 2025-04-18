d2fd743d42929d1d28d59352822f4df7e7b97388
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-04-18 12:50:49 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -58,7 +58,7 @@
 		    . ' <a href="https://www.youtube.com/@DungeonCrawlCosplayShorts" target="_blank">@DungeonCrawlCosplayShorts</a> ' 
 		    . _("is a repertoire of short video extracts that have links to the full video or playlist.") . "</p>";
 
-	echo "<p>"  . _("See the") . ' <a href="/cc_streamers"><b>' . _("Crawl Cosplay Streamers") . "</b></a>'"  
+	echo "<p>"  . _("See the") . ' <a href="/cc_streamers"><b>' . _("Crawl Cosplay Streamers") . "</b></a>' "  
 		    . _("webpage for info on current and past streamers as well as links to their CC playlists.") . "</p>";
 	echo "<p>" . _("Happy Crawling,") . "</p>";
 	echo "<p>" . _("from RoGGa your host, our webdev team, and the @VIPs members who all help make this site what it is.") . " :-D</p>";
