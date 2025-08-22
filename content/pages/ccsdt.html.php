58e675f604a1adcf20ca0f36de949ae8fa1ed34e
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-08-22 10:42:45 -0400

    Update ccsdt.html.php

--- content/pages/ccsdt.html.php
+++ content/pages/ccsdt.html.php
@@ -8,7 +8,7 @@
    you can test yourself at: <a href="https://www.crawlcosplay.org/ccsdt/0.33test/index.html">https://www.crawlcosplay.org/ccsdt/0.33test/index.html</a></p>
 
 <h2>Past Tournaments Results</h2>
-<ul><a href="./0.32/standings.html">CCSDT#1</a> for DCSS v0.32</ul>
+<ul><a href="/ccsdt/0.32/standings.html">CCSDT#1</a> for DCSS v0.32</ul>
 
 <h2>General Rules, Scoring and Credits</h2>
 See the <a href="/ccsdt/about_ccsdt.html">CCSDT About page</a>.
