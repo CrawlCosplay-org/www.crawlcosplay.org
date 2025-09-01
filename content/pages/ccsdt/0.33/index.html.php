fa269c2cb4779286d34ee2c31a5fdcc798c11813
RoGGa <robert_gagne69@yahoo.com>
2025-09-01 07:38:10 -0400

    Automated commit via ccsdt-cronjob.sh

---
+++ content/pages/ccsdt/0.33/index.html.php
@@ -0,0 +1,51 @@
+<title>Crawl Cosplay Sudden Death Tournament</title>
+    <?php $this->layout = 'ccsdt'; ?>
+    <div id="title">
+    <h2 id="sdc"><center>CCSDT for DCSS v0.33</center></h2>
+    <h2></h2></div><div id="content">
+    <pre id="cover">
+_You are suddenly pulled into a different region of the Abyss!
+_A floating eye, a glowing orange brain and 8 large abominations come into view.</pre>
+<p>The Crawl Cosplay Sudden Death Tournament (CCSTD) is a competition 
+that aims to fill a
+Crawl niche not currently filled by the version release tournament 
+and the other Crawl Cosplay initiatives.
+The idea is for players to compete by trying to do as well as possible in a
+game of Crawl with one attempt only; if you die, that week's challenge is over (thus
+"sudden death".)* 
+This competition is a lower time commitment event that aims to challenge players while
+simultaneously encouraging unusual characters and play styles that you might not normally consider.</p>
+
+<h2>Competition Format</h2>
+
+<ul>
+<li>Each challenge consists of playing a randomly chosen Crawl combo.</li>
+<li>You get <em>one</em> attempt to play each combo.*</li>
+<li>The goal is to advance as far as possible (and win!) in each game, scoring
+points by reaching various in-game milestones.</li>
+<li>Only games played and milestones scored between 00:00 UTC on the start and end dates count.</li>
+<li>Details on rules and scoring are available on the <a
+href="../about_ccsdt.html">details/about page</a>.</li>
+</ul>
+
+<p>* There is 1 caveat detailed on the details/about page at item 4.</p>
+
+<h2>Schedule</h2>
+
+<ul id="schedule"><li><span class="label">Week 1:</span> 2025-09-05 to 2025-09-12<li><span class="label">Week 2:</span> 2025-09-12 to 2025-09-19<li><span class="label">Week 3:</span> 2025-09-19 to 2025-09-26<li><span class="label">Week 4:</span> 2025-09-26 to 2025-10-03<li><span class="label">Week 5:</span> 2025-10-03 to 2025-10-10</ul>
+
+
+<h2>Sign Up</h2>
+
+<p>In order to sign up, set the first line of your 0.33 RC file to</p> 
+<pre id="rc"># ccsdt</pre>
+<p>on <a href="https://crawl.develz.org/play.htm">any of the official online servers</a>
+or CCO ( https://cco.crawlcosplay.org/ ) before the end of the first week. Your name will appear in the standings once
+you've done this correctly and started at least one 0.33 game (though it may take about 10 minutes before it does).</p>
+
+<h2>Credits</h2>
+
+<p>Original CSDC rules and organization by <a href="http://crawl.akrasiac.org/scoring/players/walkerboh.html">WalkerBoh</a> and 
+<a href="http://crawl.akrasiac.org/scoring/players/ebering.html">ebering</a>.
+Thank you to scrubbdaddy for python assistance. I am your host, RoGGa.</div>
+    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu">Week 2 - </span><span class="menu">Week 3 - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span>
