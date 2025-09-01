c43c444efe719e0ab798de63aa63c48189562a99
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-09-01 07:44:52 -0400

    Update ccsdt.html.php

--- content/pages/ccsdt.html.php
+++ content/pages/ccsdt.html.php
@@ -2,10 +2,9 @@
 <h2 style="color:rgb(69, 136, 5);">Welcome to the <b>Crawl Cosplay Sudden Death Tournament</b> (CCSDT)</h2>
 
 <h2>Next Tournament</h2>
-<ul>Next CCSDT for DCSS v0.33 starts on Friday Sept 5th, 2025 at 00:00 UTC.</ul>
-
-<p>We are currently testing small changes to the automated python scripts; 
-   you can test yourself at: <a href="https://www.crawlcosplay.org/ccsdt/0.33test/index.html">https://www.crawlcosplay.org/ccsdt/0.33test/index.html</a></p>
+<ul><li>CCSDT#2 for DCSS v0.33 starts on Friday Sept 5th, 2025 at 00:00 UTC.</li>
+    <li><a href="/ccsdt/0.33/standings.html">CCSDT#2 Standings</a> for DCSS v0.33</li>
+</ul>
 
 <h2>Past Tournaments Results</h2>
 <ul><a href="/ccsdt/0.32/standings.html">CCSDT#1</a> for DCSS v0.32</ul>
