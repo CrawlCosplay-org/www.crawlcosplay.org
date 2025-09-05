9af6c3b2dd6b19c56552d03d1745a065487e9c05
RoGGa <robert_gagne69@yahoo.com>
2025-09-04 20:52:00 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.33/1.html.php
+++ content/pages/ccsdt/0.33/1.html.php
@@ -18,9 +18,9 @@
     <th>Bonus 1</th>
     <th>Bonus 2</th>
     <th>Total</th>
-    </tr><tr class="alive"><td class="name">Hsik</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
+    </tr><tr class="alive"><td class="name">RoGGa</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">2</td></tr>
+<tr class="alive"><td class="name">Hsik</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
 <tr class="dead"><td class="name"><a href="https://cbro.berotato.org/morgue//BeansWater/morgue-BeansWater-20250905-002510.txt">BeansWater</a></td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
-<tr class="alive"><td class="name">RoGGa</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
 <tr class="none"><td class="name">Blyxx</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Bronksi</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Syis</td>
@@ -35,6 +35,7 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">ContrarianOrange</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Platt</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">tmt</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">planckenstein</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">ASCIIPhilia</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">DWEMPhilia</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Caterwaul</td>
@@ -45,4 +46,4 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">kickassninja</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">cpf86</td>
                 <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu">Week 2 - </span><span class="menu">Week 3 - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2025-09-05 00:42 UTC</span></div>
+    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu">Week 2 - </span><span class="menu">Week 3 - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2025-09-05 00:52 UTC</span></div>
