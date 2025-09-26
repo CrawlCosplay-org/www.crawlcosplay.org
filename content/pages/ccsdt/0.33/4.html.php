d2befec26b7e25f6677d297f81163a02740c61c1
RoGGa <robert_gagne69@yahoo.com>
2025-09-25 20:12:34 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.33/4.html.php
+++ content/pages/ccsdt/0.33/4.html.php
@@ -18,7 +18,8 @@
     <th>Bonus 1</th>
     <th>Bonus 2</th>
     <th>Total</th>
-    </tr><tr class="none"><td class="name">RoGGa</td>
+    </tr><tr class="alive"><td class="name">PieDemon</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
+<tr class="none"><td class="name">RoGGa</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">SlowGraham</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Pigkeeper</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Blyxx</td>
@@ -44,7 +45,6 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Hsik</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Esekla</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">community</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">PieDemon</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">agentchuck</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">TheMeInTeam</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">quinnr</td>
@@ -116,4 +116,4 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">playerghost</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">person50605</td>
                 <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2025-09-26 00:02 UTC</span></div>
+    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2025-09-26 00:12 UTC</span></div>
