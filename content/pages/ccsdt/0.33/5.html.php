6c53be4540a56e721a3cf352fdc662e1954f175c
RoGGa <robert_gagne69@yahoo.com>
2025-10-03 04:42:33 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.33/5.html.php
+++ content/pages/ccsdt/0.33/5.html.php
@@ -28,6 +28,7 @@
 <tr class="alive"><td class="name">ousd</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
 <tr class="dead"><td class="name"><a href="https://crawl.project357.org/morgue/Abbadon/morgue-Abbadon-20251003-062531.txt">Abbadon</a></td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
 <tr class="alive"><td class="name">jaccarmac</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
+<tr class="alive"><td class="name">IjonTichy1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
 <tr class="none"><td class="name">RoGGa</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">SlowGraham</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Pigkeeper</td>
@@ -74,7 +75,6 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">mobilemanners</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Arcadio</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Fougere</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">IjonTichy1</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Masnef</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Aka8171</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">planckenstein</td>
@@ -116,4 +116,4 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">playerghost</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">person50605</td>
                 <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2025-10-03 08:32 UTC</span></div>
+    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2025-10-03 08:42 UTC</span></div>
