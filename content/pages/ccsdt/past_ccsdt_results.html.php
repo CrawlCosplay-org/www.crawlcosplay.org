9dc5ac4c972ddeea86c68ab164574ed8e1dce301
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-10-10 08:27:48 -0400

    Update past_ccsdt_results.html.php

--- content/pages/ccsdt/past_ccsdt_results.html.php
+++ content/pages/ccsdt/past_ccsdt_results.html.php
@@ -2,4 +2,6 @@
 
 <h2>Past Crawl Cosplay Suddent Death Tournament (CCSDT) results</h2>
 
-<ul><li><a href="/ccsdt/0.32/standings.html">CCSDT#1 results</a> for v0.32.</li></ul>
+<ul><li><a href="/ccsdt/0.32/standings.html">CCSDT#1 results</a> for v0.32.</li>
+    <li><a href="/ccsdt/0.33/standings.html">CCSDT#2 results</a> for v0.33.</li>
+</ul>
