a3cbbbf3d75e549a78aed9c0b64bb996de0faaf4
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-10-26 09:43:15 -0400

    Update cc_streamers.html.php

--- content/pages/cc_streamers.html.php
+++ content/pages/cc_streamers.html.php
@@ -37,7 +37,7 @@
 	<li>Crawl Cosplay playlists: 	<a href="https://www.youtube.com/playlist?list=PLU5PEZPkft5vBlJp5H6e4WQUa-W5DV--m" target="_blank">CCC</a></li>
 </ul>
 
-<p><b>Platt_Mallar</b>: Mr sPlatt with 1 WIN!!! And don't forget to say Hi to Mrs Platt ;-)</p>
+<p><b>Mr and Mrs Platt</b>: both with 1 WIN!!!</p>
 <ul>	<li>Active Channels:		<a href="https://www.twitch.tv/platt_mallar" target="_blank">www.twitch.tv/platt_mallar</a> &
 									<a href="https://www.youtube.com/@platt_mallar" target="_blank">www.youtube.com/@platt_mallar</a></li>
 	<li>Mr sPlatt normally streams on Saturdays and Sundays.</li>
