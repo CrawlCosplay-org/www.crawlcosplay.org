c0e0899b7a471214a0cf371a9bf810045ba370ba
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-12-04 06:56:41 -0500

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -28,7 +28,7 @@
 
    	echo '<h3><a href="/cctt">' . _("Crawl Cosplay Trunk Tournament") . "</a> (CCTT) - LIVE</h3>";
    	echo "<ul><li>" . _("A DCSS tournament lasting about a month with each week highlighting some of the latest Trunk changes.") . "</li>";
-	echo     "<li>" . _("CCTT#3 for trunk v0.34 started on the 28th of November 2025. and runs until the start of January 2nd.") . "</li>";
+	echo     "<li>" . _("CCTT#3 for trunk v0.34 started on the 28th of November 2025 and runs until the start of January 2nd.") . "</li>";
    	echo     "<li>" . _("Read") . ': <a href="/cctt/about_cctt">' . _("About CCTT") . "</a>.</li>";
    	echo 	 '<li><a href="/cctt/past_cctt_results">' . _("Past CCTT Results") . "</a></li></ul>";
 
