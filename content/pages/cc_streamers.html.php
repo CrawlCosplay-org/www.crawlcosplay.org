71cbad7d1dc51d5d32fc4277fddf6fa71a206022
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-01-19 11:19:52 -0500

    Update cc_streamers.html.php

--- content/pages/cc_streamers.html.php
+++ content/pages/cc_streamers.html.php
@@ -17,24 +17,11 @@
 	<li>He has also been one of the few to complete !progress (winning all 665 playable DCSS combos). </li>
 </ul>
 
-<p><b>Dumalacath</b>: CCA Master and aspiring CCC demi-god!</p>
-<ul>	<li>Non-Active Channel:		<a href="https://www.youtube.com/@dumalacath" target="_blank">www.youtube.com/@dumalacath</a></li>
-	<li>Crawl Cosplay playlists: 	<a href="https://www.youtube.com/playlist?list=PLy8-17WgznzHupXlm9SbgNVWfeF46Yvku" target="_blank">CCA</a>; 
-					<a href="https://www.youtube.com/playlist?list=PLy8-17WgznzGYSC6dJy6NqHErLrtvoYRR" target="_blank">CCC</a>; 
-					<a href="https://www.youtube.com/playlist?list=PLy8-17WgznzHBfUlTlz_ScV5_irFeNx8C" target="_blank">CCTT</a></li>
-</ul>
-
 <p><b>Arcadio</b> (in french): le semi-pro et demi-dieu des roguelikes!</p>
 <ul>	<li>Active Channels:		<a href="https://www.twitch.tv/arcadiorl" target="_blank">www.twitch.tv/arcadiorl</a> & 
 					<a href="https://www.youtube.com/@arcadiorl" target="_blank">www.youtube.com/@arcadiorl</a> & 
 					<a href="https://discord.gg/p8S4sb8yab" target="_blank">Discord server</a></li>
 	<li>Crawl Cosplay playlists: 	<a href="https://www.youtube.com/playlist?list=PLOHsyPzEhlNSOcOw2njsqfN5eQ8KJ4is5" target="_blank">CCTT</a></li>
-</ul>
-
-<p><b>Wizard1ke</b>: our favourite Kiwi</p>
-<ul>	<li>Non-Active Channels:	<a href="https://www.twitch.tv/wizardike" target="_blank">www.twitch.tv/wizardike</a> & 
-					<a href="https://www.youtube.com/@WizardIke" target="_blank">www.youtube.com/@WizardIke</a></li>
-	<li>Crawl Cosplay playlists: 	<a href="https://www.youtube.com/playlist?list=PLU5PEZPkft5vBlJp5H6e4WQUa-W5DV--m" target="_blank">CCC</a></li>
 </ul>
 
 <p><b>Mr and Mrs Platt</b>: both with 1 WIN!!!</p>
@@ -43,16 +30,29 @@
 	<li>Mr sPlatt normally streams on Saturdays and Sundays.</li>
 </ul>
 
+<p><b>Draconius</b>: with 84 GnIE wins!!!</p>
+<ul>	<li>Active Channel:		<a href="https://www.youtube.com/@DraconiusRogueLike" target="_blank">www.youtube.com/@DraconiusRogueLike</a></li>
+</ul>
+
+<p><b>Dumalacath</b>: CCA Master and aspiring CCC demi-god!</p>
+<ul>	<li>Non-Active Channel:		<a href="https://www.youtube.com/@dumalacath" target="_blank">www.youtube.com/@dumalacath</a></li>
+	<li>Crawl Cosplay playlists: 	<a href="https://www.youtube.com/playlist?list=PLy8-17WgznzHupXlm9SbgNVWfeF46Yvku" target="_blank">CCA</a>; 
+					<a href="https://www.youtube.com/playlist?list=PLy8-17WgznzGYSC6dJy6NqHErLrtvoYRR" target="_blank">CCC</a>; 
+					<a href="https://www.youtube.com/playlist?list=PLy8-17WgznzHBfUlTlz_ScV5_irFeNx8C" target="_blank">CCTT</a></li>
+</ul>
+
 <p><b>ContrarianOrange</b>: the cautious with 2 wins!</p>
 <ul>	<li>Active Channels:		<a href="https://www.twitch.tv/contrarianorange" target="_blank">www.twitch.tv/contrarianorange</a></li>
 	<li>Plays at random times but more often than not during the evenings eastern timezone.</li>
 </ul>
 
+<p><b>Wizard1ke</b>: our favourite Kiwi</p>
+<ul>	<li>Non-Active Channels:	<a href="https://www.twitch.tv/wizardike" target="_blank">www.twitch.tv/wizardike</a> & 
+					<a href="https://www.youtube.com/@WizardIke" target="_blank">www.youtube.com/@WizardIke</a></li>
+	<li>Crawl Cosplay playlists: 	<a href="https://www.youtube.com/playlist?list=PLU5PEZPkft5vBlJp5H6e4WQUa-W5DV--m" target="_blank">CCC</a></li>
+</ul>
+
 <p><b>Planckenstein</b>: Mr CCSDT#2!</p>
 <ul>	<li>Active Channels:		<a href="https://www.twitch.tv/planckenstein" target="_blank">www.twitch.tv/planckenstein</a> &
 									<a href="https://www.youtube.com/@planckenstein" target="_blank">www.youtube.com/@planckenstein</a></li>
-</ul>
-
-<p><b>Draconius</b></p>
-<ul>	<li>Active Channel:		<a href="https://www.youtube.com/@DraconiusRogueLike" target="_blank">www.youtube.com/@DraconiusRogueLike</a></li>
 </ul>
