6cf7653fa4d7ef2c05d526277375d567c8b6e766
RoGGa <robert_gagne69@yahoo.com>
2026-05-04 14:37:29 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.34test/2.html.php
+++ content/pages/ccsdt/0.34test/2.html.php
@@ -1,7 +1,7 @@
 <title>Week 2 &mdash; TeFE</title>
     <?php $this->layout = 'ccsdt'; ?>
     <meta http-equiv="refresh" content="240"><div id="title">
-    <h2 id="sdc"><center>CCSDT#3 (testing) for DCSS v0.34</center></h2>
+    <h2 id="sdc"><center>Testing prior to CCSDT#3 for DCSS v0.34</center></h2>
     <h2>Week 2 &mdash; TeFE</h2></div><div id="content"><div id="times"><span class="label">Week of 2026-04-24</span></div><div id="combo"><span class="label">Character: </span>Tengu Fire Elementalist</div>
 <div id="bonus"><span class="label">Bonus 1: </span>Reach the end of the Depths before entering a rune branch (excluding getting banished to the Abyss).<br/>
 <span class="label">Bonus 2: </span>Get a rune from Hell or Pan before entering any other rune branch (excluding the Abyss).</div>
@@ -31,4 +31,4 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Caterwaul</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">krsz</td>
                 <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu">Week 2 - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-04 18:27 UTC</span></div>
+    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu">Week 2 - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-04 18:37 UTC</span></div>
