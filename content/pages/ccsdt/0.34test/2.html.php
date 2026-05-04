8f83146810df9d2687c4a28cc218a31e634f635d
RoGGa <robert_gagne69@yahoo.com>
2026-05-04 19:27:37 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.34test/2.html.php
+++ content/pages/ccsdt/0.34test/2.html.php
@@ -7,20 +7,21 @@
 <span class="label">Bonus 2: </span>Get a rune from Hell or Pan before entering any other rune branch (excluding the Abyss).</div>
 <div id="gods"><span class="label">Gods: </span>Sif Muna, Sif Muna, Sif Muna</div><br><pre>LEGEND<br>------<br>Green = Won<br>Red   = Died<br>Grey  = ongoing or did not finish before end of the week</right></pre><div class="card"><table><tr class="head">
     <th>Player</th>
-#    <th>Enter Temple</th>
+    <th>Reach XL5</th>
     <th>Unique Kill</th>
+    <th>Enter Temple</th>
     <th>Branch Enter</th>
     <th>Branch End</th>
     <th>Champion God</th>
-#    <th>Collect a Gem</th> 
+    <th>Collect a Gem</th> 
     <th>Collect a Rune</th>
-#    <th>Collect 2 </th>
+    <th>Collect 2 Runes</th>
     <th>Collect 3 Runes</th>
     <th>Obtain the Orb</th>
     <th>Win</th>
     <th>Bonus 1</th>
     <th>Bonus 2</th>
-    <th>Total</th>
+    <th>Total (max=15)</th>
     </tr><tr class="none"><td class="name">RoGGa</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">agentchuck</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">TheMeInTeam</td>
@@ -34,4 +35,4 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Caterwaul</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">krsz</td>
                 <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu">Week 2 - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-04 23:17 UTC</span></div>
+    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu">Week 2 - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-04 23:27 UTC</span></div>
