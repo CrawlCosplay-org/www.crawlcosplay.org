4a55ad894f6546f793314adf818c82e0943cfaa1
RoGGa <robert_gagne69@yahoo.com>
2026-05-05 07:47:30 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.34test/rules.html.php
+++ content/pages/ccsdt/0.34test/rules.html.php
@@ -41,23 +41,27 @@ games achieve them.</p>
 
 <table class="info"><tr class="head"><th>Weekly points (can be earned each
 week)</th><th></th></tr>
-<tr><td class="name">Kill a unique:</td><td class="pt">1</td></tr>
-<tr><td class="name">Enter a multi-level branch of the dungeon:</td><td class="pt">1</td></tr>
-<tr><td class="name">Reach the end of a multi-level branch (including D):</td><td class="pt">1</td></tr>
-<tr><td class="name">Champion a listed god:</td><td class="pt">1</td></tr>
-<tr><td class="name">Collect a rune:</td><td class="pt">1</td></tr>
-<tr><td class="name">Collect 3 or more runes:</td><td class="pt">1</td></tr>
-<tr><td class="name">Collect the Orb of Zot:</td><td class="pt">1</td></tr>
-<tr><td class="name">Win:</td><td class="pt">1</td></tr>
-<tr><td class="name">Complete Bonus 1:</td><td class="pt">1</td></tr>
-<tr><td class="name">Complete Bonus 2:</td><td class="pt">1</td></tr>
+<tr><td class="name"> 1. Kill a unique</td><td class="pt">1</td></tr>
+<tr><td class="name"> 2. Reach XL 5</td><td class="pt">1</td></tr>
+<tr><td class="name"> 3. Worship one of three gods</td><td class="pt">1</td></tr>
+<tr><td class="name"> 4. Enter a multi-level branch of the dungeon</td><td class="pt">1</td></tr>
+<tr><td class="name"> 5. Reach the end of a multi-level branch (including D)</td><td class="pt">1</td></tr>
+<tr><td class="name"> 6. Champion a listed god</td><td class="pt">1</td></tr>
+<tr><td class="name"> 7. Collect 1 rune</td><td class="pt">1</td></tr>
+<tr><td class="name"> 8. Collect 2 runes</td><td class="pt">1</td></tr>
+<tr><td class="name"> 9. Collect 3 runes</td><td class="pt">1</td></tr>
+<tr><td class="name">10. Collect 1 gem</td><td class="pt">1</td></tr>
+<tr><td class="name">11. Collect the Orb of Zot</td><td class="pt">1</td></tr>
+<tr><td class="name">12. Win</td><td class="pt">1</td></tr>
+<tr><td class="name">13. Complete Bonus 1</td><td class="pt">1</td></tr>
+<tr><td class="name">14. Complete Bonus 2</td><td class="pt">2</td></tr>
 <tr class="head" id="onetime"><th>One-time points (earned once during the tournament)</th><th></th></tr>
 <!-- <tr><td class="name">Win a game in under 40,000 turns:</td><td class="pt">2</td></tr> -->
-<tr><td class="name">Win a game with 15 runes:</td><td class="pt">3</td></tr>
-<tr><td class="name">Clear a Ziggurat:</td><td class="pt">4</td></tr>
-<tr><td class="name">Enter Zot at XL 20 or lower:</td><td class="pt">5</td></tr>
-<tr><td class="name">Win a game without entering lair:</td><td class="pt">6</td></tr>
-<tr><td class="name">Get a rune without using potions or scrolls:</td><td class="pt">7</td></tr>
+<tr><td class="name">Win a game with 15 runes</td><td class="pt">3</td></tr>
+<tr><td class="name">Clear a Ziggurat</td><td class="pt">4</td></tr>
+<tr><td class="name">Enter Zot at XL 20 or lower</td><td class="pt">5</td></tr>
+<tr><td class="name">Win a game without entering lair</td><td class="pt">6</td></tr>
+<tr><td class="name">Get a rune without using potions or scrolls (aesetic rune)</td><td class="pt">7</td></tr>
 </table>
 
 <!-- <p class="notes"> Unless specified, a bonus or one time point does not
