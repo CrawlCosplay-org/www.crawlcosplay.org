6ce6c9926aa3a48b73e83c799db8758aeca92280
RoGGa <robert_gagne69@yahoo.com>
2026-05-06 04:07:47 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.34test/rules.html.php
+++ content/pages/ccsdt/0.34test/rules.html.php
@@ -73,6 +73,6 @@ week)</th><th>Points</th></tr>
 <tr><td class="name">4. Win a game without entering lair</td><td class="pt">6</td></tr>
 <tr><td class="name">5. Get a rune without using potions or scrolls (aesetic rune)</td><td class="pt">7</td></tr>
 </table>
-<p>4 new milestones and 1 scoring change for CCSDT#3.
+<p>* 4 new milestones and 1 scoring change for CCSDT#3.
 </div>
     <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span></div>
