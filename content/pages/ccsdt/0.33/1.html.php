faa080c8ad78fc6fc50ba1f89ef25c8d929b9ce3
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-05-06 10:47:29 -0400

    Update 1.html.php

--- content/pages/ccsdt/0.33/1.html.php
+++ content/pages/ccsdt/0.33/1.html.php
@@ -126,7 +126,7 @@
         <span class="menu"><a href="rules.html">Details</a> - </span>
         <span class="menu"><a href="standings.html">Standings</a></span>
         <span class="menuspacer"></span>
-        <span class="menu">Week 1 - </span>
+        <span class="menu"><a href="1.html">Week 1</a> - </span>
         <span class="menu"><a href="2.html">Week 2</a> - </span>
         <span class="menu"><a href="3.html">Week 3</a> - </span>
         <span class="menu"><a href="4.html">Week 4</a> - </span>
