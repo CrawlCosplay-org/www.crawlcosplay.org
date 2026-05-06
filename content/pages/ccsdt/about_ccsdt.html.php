417fc68e161439f33a30d93a0154182e4c480e0f
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-05-06 05:43:31 -0400

    Update about_ccsdt.html.php

--- content/pages/ccsdt/about_ccsdt.html.php
+++ content/pages/ccsdt/about_ccsdt.html.php
@@ -42,20 +42,21 @@ played, and one-time points awarded once per season regardless of how many
 games achieve them.</p>
 
 <table class="info"><tr class="head"><th>15 Weekly Points (can be earned each week)*</th><th>Points</th></tr>
-<tr><td class="name"> 1. Kill a Unique:</td><td class="pt">1     </td></tr>
-<tr><td class="name"> 2. Reach XL5:</td><td class="pt">1 (new)</td></tr>
+<tr><td class="name"> 1. Reach XL5:</td><td class="pt">1 (new)</td></tr>
+<tr><td class="name"> 2. Kill a Unique:</td><td class="pt">1</td></tr>
 <tr><td class="name"> 3. Worship a listed god:</td><td class="pt">1 (new)</td></tr>
-<tr><td class="name"> 4. Enter a multi-level branch of the dungeon:</td><td class="pt">1</td></tr>
-<tr><td class="name"> 5. Reach the end of a multi-level branch (including Dungeon):</td><td class="pt">1</td></tr>
-<tr><td class="name"> 6. Champion a listed god:</td><td class="pt">1</td></tr>
-<tr><td class="name"> 7. Collect a Rune:</td><td class="pt">1</td></tr>
-<tr><td class="name"> 8. Collect a 2nd Rune:</td><td class="pt">1 (new)</td></tr>
-<tr><td class="name"> 9. Collect 3 or more Runes:</td><td class="pt">1</td></tr>
-<tr><td class="name">10. Collect a Gem:</td><td class="pt">1 (new)</td></tr>
-<tr><td class="name">11. Pick up the Orb of Zot:</td><td class="pt">1</td></tr>
-<tr><td class="name">12. Win:</td><td class="pt">1</td></tr>
-<tr><td class="name">13. Complete Bonus 1:</td><td class="pt">1</td></tr>
-<tr><td class="name">14. Complete Bonus 2:</td><td class="pt">2</td></tr>
+<tr><td class="name"> 4. Reach XL10:</td><td class="pt">1 (new)</td></tr>
+<tr><td class="name"> 5. Enter a multi-level branch of the dungeon:</td><td class="pt">1</td></tr>
+<tr><td class="name"> 6. Reach the end of a multi-level branch (including Dungeon):</td><td class="pt">1</td></tr>
+<tr><td class="name"> 7. Champion a listed god:</td><td class="pt">1</td></tr>
+<tr><td class="name"> 8. Collect a Gem:</td><td class="pt">1 (new)</td></tr>
+<tr><td class="name"> 9. Collect a Rune:</td><td class="pt">1</td></tr>
+<tr><td class="name">10. Collect a 2nd Rune:</td><td class="pt">1 (new)</td></tr>
+<tr><td class="name">11. Collect 3 or more Runes:</td><td class="pt">1</td></tr>
+<tr><td class="name">12. Pick up the Orb of Zot:</td><td class="pt">1</td></tr>
+<tr><td class="name">13. Win:</td><td class="pt">1</td></tr>
+<tr><td class="name">14. Complete Bonus 1:</td><td class="pt">1</td></tr>
+<tr><td class="name">15. Complete Bonus 2:</td><td class="pt">1</td></tr>
 <tr class="head" id="onetime"><th>One-time points (earned once during the tournament)**</th><th>Points</th></tr>
 <!-- <tr><td class="name">Win a game in under 40,000 turns:</td><td class="pt">2</td></tr> -->
 <tr><td class="name">1. Win a game with 15 runes:</td><td class="pt">3</td></tr>
@@ -64,8 +65,8 @@ games achieve them.</p>
 <tr><td class="name">4. Win a game without entering lair:</td><td class="pt">6</td></tr>
 <tr><td class="name">5. Get a rune without using potions or scrolls:</td><td class="pt">7</td></tr>
 </table>
-<p class="notes"> * The scoring of weekly points was reworked for CCSDT#3 and 4 item were added. *** The new ones have yet to be programmed. ***</p>
-<p class="notes"> ** The scoring of one-time points was reworked for CCSDT#3 and 1 item was removed. </p>
+<p class="notes"> * The scoring of weekly points was reworked for CCSDT#3 and 5 milestones were added.</p>
+<p class="notes"> ** The scoring of one-time points was reworked for CCSDT#3 and 1 item was removed (Win <40k Turns). </p>
 
 <h2>List of possible Bonuses</h2>
 
