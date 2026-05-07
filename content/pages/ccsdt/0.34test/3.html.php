bcac72954f319fd572be21afec471142e081d8dc
RoGGa <robert_gagne69@yahoo.com>
2026-05-07 11:53:46 -0400

    Manual Update

---
+++ content/pages/ccsdt/0.34test/3.html.php
@@ -0,0 +1,45 @@
+<title>Week 3 &mdash; Dispater (GrEE)</title>
+    <?php $this->layout = 'ccsdt'; ?>
+    <meta http-equiv="refresh" content="240"><div id="title">
+    <h2 id="sdc"><center>Testing prior to CCSDT#3 for DCSS v0.34</center></h2>
+    <h2>Week 3 &mdash; Dispater (GrEE)</h2></div><div id="content"><div id="times"><span class="label">Week of: </span>2026-05-01 (ends at start of 2026-05-08 00:00 UTC)</div><div id="combo"><span class="label">Character: </span>Gargoyle Earth Elementalist</div>
+<div id="bonus"><span class="label">Bonus 1: </span>Enter Slime as your second multi-level branch (don't get banished).<br/>
+<span class="label">Bonus 2: </span>Enter a rune branch with all base skills < 11.</div>
+<div id="gods"><span class="label">Gods: </span>Makhleb, Makhleb, Makhleb</div><br><pre>LEGEND<br>------<br>Green = Won<br>Red   = Died<br>Grey  = ongoing or did not finish before end of the week</right></pre><div class="card"><table><tr class="head">
+    <th>Player</th>
+    <th>Reach XL5</th>
+    <th>Kill a Unique</th>
+    <th>Worship a Valid God</th>
+    <th>Reach XL10</th>
+    <th>Branch Enter</th>
+    <th>Branch End</th>
+    <th>Champion a Valid God</th>
+    <th><abbr title="Does not need to stay intact">Collect 1 Gem</abbr></th>
+    <th>Collect 1 Rune</th>
+    <th>Collect 2 Runes</th>
+    <th>Collect 3 Runes</th>
+    <th>Obtain the Orb</th>
+    <th>Win</th>
+    <th>Bonus #1</th>
+    <th>Bonus #2</th>
+    <th>Total (max=15)</th>
+    </tr><tr class="alive"><td class="name">Blyxx</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">1</td><td class="pt">1</td><td class="total">11</td></tr>
+<tr class="alive"><td class="name">agentchuck</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">1</td><td class="pt">1</td><td class="total">11</td></tr>
+<tr class="alive"><td class="name">RoGGaTest2</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">1</td><td class="pt">1</td><td class="total">11</td></tr>
+<tr class="dead"><td class="name"><a href="https://cbro.berotato.org/morgue//RoGGa/morgue-RoGGa-20260502-160909.txt">RoGGa</a></td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">4</td></tr>
+<tr class="alive"><td class="name">ElricMelnibone</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">4</td></tr>
+<tr class="dead"><td class="name"><a href="http://crawl.xtahua.com/crawl/morgue/Kergan/morgue-Kergan-20260506-201512.txt">Kergan</a></td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">3</td></tr>
+<tr class="dead"><td class="name"><a href="http://crawl.xtahua.com/crawl/morgue/Colgate/morgue-Colgate-20260504-165012.txt">Colgate</a></td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">2</td></tr>
+<tr class="alive"><td class="name">RoGGaTestCBRG</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
+<tr class="none"><td class="name">Jannitor</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Salticid</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">TheMeInTeam</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Astinus</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Platt</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Archion</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">ASCIIPhilia</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">yakattack</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Caterwaul</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">krsz</td>
+                <td colspan="15"></td><td class="total">0</td></tr></table></div></div>
+    <div id="bottomtext"><span class="menu">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu">Week 3 - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-07 15:53 UTC</span></div>
