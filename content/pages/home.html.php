2b8e471aa530f5cff529e6e84df5d903242ab79d
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-05-07 19:18:02 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -28,7 +28,7 @@
 
 	echo '<h3><a href="/ccsdt">' . _("Crawl Cosplay Sudden Death Tournament") . "</a> (CCSDT) - COMING SOON</h3>";
 	echo "<ul><li>" . _("A DCSS tournament lasting 5 weeks with a different Crawl Unique combo to play each week. You only get one try!") . "</li>";
-	echo     "<li>" . _("CCSDT#3 for DCSS v0.34 starts on Friday May 15th, 2026.") . "</li>";
+	echo     "<li>" . _("CCSDT#3 for DCSS v0.34 starts on Friday May 15th, 2026.") . "</li></ul>";
 
    	echo '<h3><a href="/cctt">' . _("Crawl Cosplay Trunk Tournament") . "</a> (CCTT)</h3>";
    	echo "<ul><li>" . _("A DCSS tournament lasting about a month with each week highlighting some of the latest Trunk changes.") . "</li>";
