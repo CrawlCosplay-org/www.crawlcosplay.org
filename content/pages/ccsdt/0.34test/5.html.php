f7491379b1845f039905cb87a23441f8c89b2295
RoGGa <robert_gagne69@yahoo.com>
2026-05-08 10:03:34 -0400

    Semi-Manual Update

--- content/pages/ccsdt/0.34test/5.html.php
+++ content/pages/ccsdt/0.34test/5.html.php
@@ -1,32 +1,50 @@
-<title>Week 5 &mdash; TeFE</title>
+<title>Week 5 &mdash; Geryon (NaGl)</title>
     <?php $this->layout = 'ccsdt'; ?>
     <meta http-equiv="refresh" content="240"><div id="title">
-    <h2 id="sdc"><center>CCSDT#3 for DCSS v0.34test</center></h2>
-    <h2>Week 5 &mdash; TeFE</h2></div><div id="content"><div id="times"><span class="label">Week of 2025-10-03</span></div><div id="combo"><span class="label">Character: </span>Tengu Fire Elementalist</div>
-<div id="bonus"><span class="label">Bonus 1: </span>Enter a rune branch with all base skills < 11.<br/>
-<span class="label">Bonus 2: </span>Collect a rune in less than 15,000 turns.</div>
-<div id="gods"><span class="label">Gods: </span>Jiyva, Vehumet, Makhleb</div><br><pre>LEGEND<br>------<br>Green = Won<br>Red   = Died<br>Grey  = ongoing or did not finish</right></pre><div class="card"><table><tr class="head">
+    <h2 id="sdc"><center>Testing prior to CCSDT#3 for DCSS v0.34</center></h2>
+    <h2>Week 5 &mdash; Geryon (NaGl)</h2></div><div id="content"><div id="times"><span class="label">Week of: </span>2026-05-08 (ends at start of 2026-05-15 00:00 UTC)</div><div id="combo"><span class="label">Character: </span>Naga Gladiator</div>
+<div id="bonus"><span class="label">Bonus 1: </span>Reach the 10th floor of a Ziggurat.<br/>
+<span class="label">Bonus 2: </span>Enter the final floor of Gehenna (Geh:7) in under 27,000 turns.</div>
+<div id="gods"><span class="label">Gods: </span>Hepliaklqana, Hepliaklqana, Hepliaklqana</div><br><pre>LEGEND<br>------<br>Green = Won<br>Red   = Died<br>Grey  = ongoing or did not finish before end of the week</right></pre><div class="card"><table><tr class="head">
     <th>Player</th>
-    <th>Unique Kill</th>
+    <th>Reach XL5</th>
+    <th>Kill a Unique</th>
+    <th>Worship a Valid God</th>
+    <th>Reach XL10</th>
     <th>Branch Enter</th>
     <th>Branch End</th>
-    <th>Champion God</th>
-    <th>Collect a Rune</th>
+    <th>Champion a Valid God</th>
+    <th><abbr title="Does not need to stay intact">Collect 1 Gem</abbr></th>
+    <th>Collect 1 Rune</th>
+    <th>Collect 2 Runes</th>
     <th>Collect 3 Runes</th>
-    <th>Obtain the Orb</th>
+    <th>Obtain Orb of Zot</th>
     <th>Win</th>
-    <th>Bonus 1</th>
-    <th>Bonus 2</th>
-    <th>Total</th>
-    </tr><tr class="none"><td class="name">RoGGa</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">agentchuck</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">TheMeInTeam</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Astinus</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Platt</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Archion</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">ASCIIPhilia</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">yakattack</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">ElricMelnibone</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Caterwaul</td>
-                <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu">Week 2 - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-02 14:28 UTC</span></div>
+    <th>Bonus #1</th>
+    <th>Bonus #2</th>
+    <th>Week's Total (max=15)</th>
+    </tr><tr class="none"><td class="name">RoGGaTest</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCRG</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Jannitor</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Salticid</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">baljet</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCUE</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGa</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">agentchuck</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">TheMeInTeam</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Astinus</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Platt</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTest2</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Kergan</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Colgate</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Blyxx</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Archion</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">ASCIIPhilia</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">yakattack</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">ElricMelnibone</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Caterwaul</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCBRG</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">krsz</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCCO</td>
+                <td colspan="15"></td><td class="total">0</td></tr></table></div></div>
+    <div id="bottomtext"><span class="menu">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-08 14:03 UTC</span></div>
