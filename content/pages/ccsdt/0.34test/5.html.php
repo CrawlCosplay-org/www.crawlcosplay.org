aec5c784cc105954a815006d7a75bb4ad44241ce
RoGGa <robert_gagne69@yahoo.com>
2026-05-09 15:28:50 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.34test/5.html.php
+++ content/pages/ccsdt/0.34test/5.html.php
@@ -39,6 +39,7 @@
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Astinus</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Platt</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Myro</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">rago</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Kergan</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">THILQ</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Colgate</td>
@@ -51,4 +52,4 @@
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">krsz</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Quoth</td>
                 <td colspan="15"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu">Week 2 - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-09 19:18 UTC</span></div>
+    <div id="bottomtext"><span class="menu">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu">Week 2 - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-09 19:28 UTC</span></div>
