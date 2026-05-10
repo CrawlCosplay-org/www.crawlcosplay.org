a8ded0752dabca040bc67dbda62b20a9437aa063
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-05-10 13:32:21 -0400

    Delete content/pages/ccsdt/past_ccsdt_results.html.php

--- content/pages/ccsdt/past_ccsdt_results.html.php
+++
@@ -1,7 +0,0 @@
-<?php $this->layout = 'ccsdt'; ?>
-
-<h2>Past Crawl Cosplay Suddent Death Tournament (CCSDT) results</h2>
-
-<ul><li><a href="/ccsdt/0.32/standings.html">CCSDT#1 results</a> for v0.32.</li>
-    <li><a href="/ccsdt/0.33/standings.html">CCSDT#2 results</a> for v0.33.</li>
-</ul>
