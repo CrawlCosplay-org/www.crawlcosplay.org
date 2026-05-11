ca2d1504126fe9ae7af71219fea82f1fbb911e2d
RoGGa <robert_gagne69@yahoo.com>
2026-05-11 12:08:45 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.34test/1.html.php
+++ content/pages/ccsdt/0.34test/1.html.php
@@ -23,9 +23,9 @@
     <th>Bonus #1</th>
     <th>Bonus #2</th>
     <th>Week's Total (max=15)</th>
-    </tr><tr class="alive"><td class="name">RoGGaTest2</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
-<tr class="none"><td class="name">RoGGaTest</td>
-                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCRG</td>
+    </tr><tr class="dead"><td class="name"><a href="https://data.crawlcosplay.org/morgue//RoGGaTest2/morgue-RoGGaTest2-20260511-160054.txt">RoGGaTest2</a></td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
+<tr class="alive"><td class="name">RoGGaTest</td><td class="pt">0</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
+<tr class="none"><td class="name">RoGGaTestCRG</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Jannitor</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Salticid</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">baljet</td>
@@ -57,4 +57,4 @@
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCAO</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCDI</td>
                 <td colspan="15"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><img src="/img/animated-arrow.gif" width="40" height="10">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu"><a href="5.html">Week 5</a> - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-11 15:58 UTC</span></div>
+    <div id="bottomtext"><span class="menu"><img src="/img/animated-arrow.gif" width="40" height="10">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu"><a href="5.html">Week 5</a> - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-11 16:08 UTC</span></div>
