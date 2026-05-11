f8af9a20b2d8ad2dd9c57aeaa2f265b597ce9d14
RoGGa <robert_gagne69@yahoo.com>
2026-05-11 11:27:23 -0400

    On Demand Update

--- content/pages/ccsdt/0.34test/1.html.php
+++ content/pages/ccsdt/0.34test/1.html.php
@@ -4,7 +4,7 @@
     <h2 id="sdc"><center>Testing prior to CCSDT#3 for DCSS v0.34</center></h2>
     <h2>Week 1 &mdash; Donald (MfFi)</h2></div><div id="content"><div id="times"><span class="label">Week of: </span>2026-05-10 (ends at start of 2026-05-15 00:00 UTC)</div><div id="combo"><span class="label">Character: </span>Merfolk Fighter</div>
 <div id="bonus"><span class="label">Bonus 1: </span>Get the Abyssal rune in under 27,000 turns.<br/>
-<span class="label">Bonus 2: </span>Reach the 10th floor of a Ziggurat.</div>
+<span class="label">Bonus 2: </span>Enter Elf:3 in under 12,000 turns.</div>
 <div id="gods"><span class="label">Gods: </span>Ashenzari, Okawaru, Dithmenos</div><br><pre>LEGEND<br>------<br>Green = Won<br>Red   = Died<br>Grey  = ongoing or did not finish before end of the week</right></pre><div class="card"><table><tr class="head">
     <th>Player</th>
     <th>Reach XL5</th>
@@ -57,4 +57,4 @@
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCAO</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCDI</td>
                 <td colspan="15"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><img src="/img/animated-arrow.gif" width="40" height="10">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu"><a href="5.html">Week 5</a> - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-11 15:20 UTC</span></div>
+    <div id="bottomtext"><span class="menu"><img src="/img/animated-arrow.gif" width="40" height="10">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu"><a href="5.html">Week 5</a> - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-11 15:27 UTC</span></div>
