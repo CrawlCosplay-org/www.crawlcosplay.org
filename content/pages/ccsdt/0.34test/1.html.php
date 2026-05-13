4600e0e88dc572d3901d1c8745c27c96aaf524df
RoGGa <robert_gagne69@yahoo.com>
2026-05-13 19:38:06 -0400

    On Demand Update

--- content/pages/ccsdt/0.34test/1.html.php
+++ content/pages/ccsdt/0.34test/1.html.php
@@ -3,8 +3,8 @@
     <meta http-equiv="refresh" content="240"><div id="title">
     <h2 id="sdc"><center>Testing prior to CCSDT#3 for DCSS v0.34</center></h2>
     <h2>Week 1 &mdash; Gargoyle (GrFi)</h2></div><div id="content"><div id="times"><span class="label">Week of: </span>2026-05-10 (ends at start of 2026-05-15 00:00 UTC)</div><div id="combo"><span class="label">Character: </span>Gargoyle Fighter</div>
-<div id="bonus"><span class="label">Bonus 1: </span>Get the slimy rune.<br/>
-<span class="label">Bonus 2: </span>Get the slimy gem. (it doesn't need to stay intact)</div>
+<div id="bonus"><span class="label">Bonus 1: </span>Kill a Pan Lord unique.<br/>
+<span class="label">Bonus 2: </span>Kill a Hell Lord unique. (Geryon does not count)</div>
 <div id="gods"><span class="label">Gods: </span>Sif Muna, Okawaru, Jiyva</div><br><pre>LEGEND<br>------<br>Green = Won<br>Red   = Died<br>Grey  = ongoing or did not finish before end of the week</right></pre><div class="card"><table><tr class="head">
     <th>Player</th>
     <th>Reach XL5</th>
@@ -23,7 +23,7 @@
     <th>Bonus #1</th>
     <th>Bonus #2</th>
     <th>Week's Total (max=15)</th>
-    </tr><tr class="alive"><td class="name">RoGGaTest2</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">1</td><td class="pt">1</td><td class="total">13</td></tr>
+    </tr><tr class="won"><td class="name"><a href="https://data.crawlcosplay.org/morgue//RoGGaTest2/morgue-RoGGaTest2-20260513-233322.txt">RoGGaTest2</a></td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">1</td><td class="total">14</td></tr>
 <tr class="none"><td class="name">RoGGaTest</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCRG</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Jannitor</td>
@@ -59,4 +59,4 @@
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCAO</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCDI</td>
                 <td colspan="15"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><img src="/img/animated-arrow.gif" width="40" height="10">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu"><a href="5.html">Week 5</a> - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-13 23:29 UTC</span></div>
+    <div id="bottomtext"><span class="menu"><img src="/img/animated-arrow.gif" width="40" height="10">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu"><a href="5.html">Week 5</a> - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-13 23:38 UTC</span></div>
