8c8ff9f50bf69063365d8a00e9c56cb17a7057f4
RoGGa <robert_gagne69@yahoo.com>
2026-05-13 08:38:14 -0400

    On Demand Update

--- content/pages/ccsdt/0.34test/1.html.php
+++ content/pages/ccsdt/0.34test/1.html.php
@@ -1,11 +1,11 @@
-<title>Week 1 &mdash; Donald (MfFi)</title>
+<title>Week 1 &mdash; Asterion (MiFi)</title>
     <?php $this->layout = 'ccsdt'; ?>
     <meta http-equiv="refresh" content="240"><div id="title">
     <h2 id="sdc"><center>Testing prior to CCSDT#3 for DCSS v0.34</center></h2>
-    <h2>Week 1 &mdash; Donald (MfFi)</h2></div><div id="content"><div id="times"><span class="label">Week of: </span>2026-05-10 (ends at start of 2026-05-15 00:00 UTC)</div><div id="combo"><span class="label">Character: </span>Merfolk Fighter</div>
+    <h2>Week 1 &mdash; Asterion (MiFi)</h2></div><div id="content"><div id="times"><span class="label">Week of: </span>2026-05-10 (ends at start of 2026-05-15 00:00 UTC)</div><div id="combo"><span class="label">Character: </span>Minotaur Fighter</div>
 <div id="bonus"><span class="label">Bonus 1: </span>Get the slimy rune.<br/>
 <span class="label">Bonus 2: </span>Get the slimy gem. (it doesn't need to stay intact)</div>
-<div id="gods"><span class="label">Gods: </span>Ashenzari, Okawaru, Dithmenos</div><br><pre>LEGEND<br>------<br>Green = Won<br>Red   = Died<br>Grey  = ongoing or did not finish before end of the week</right></pre><div class="card"><table><tr class="head">
+<div id="gods"><span class="label">Gods: </span>Sif Muna, Okawaru, Dithmenos</div><br><pre>LEGEND<br>------<br>Green = Won<br>Red   = Died<br>Grey  = ongoing or did not finish before end of the week</right></pre><div class="card"><table><tr class="head">
     <th>Player</th>
     <th>Reach XL5</th>
     <th>Kill a Unique</th>
@@ -23,25 +23,23 @@
     <th>Bonus #1</th>
     <th>Bonus #2</th>
     <th>Week's Total (max=15)</th>
-    </tr><tr class="dead"><td class="name"><a href="https://crawl-br.roguelikes.gg/morgue//RoGGaTestCBRG/morgue-RoGGaTestCBRG-20260512-210901.txt">RoGGaTestCBRG</a></td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">11</td></tr>
-<tr class="alive"><td class="name">agentchuck</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">9</td></tr>
-<tr class="dead"><td class="name"><a href="https://data.crawlcosplay.org/morgue//RoGGaTestCCO/morgue-RoGGaTestCCO-20260511-174625.txt">RoGGaTestCCO</a></td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">7</td></tr>
-<tr class="dead"><td class="name"><a href="https://data.crawlcosplay.org/morgue//RoGGaTest/morgue-RoGGaTest-20260511-162504.txt">RoGGaTest</a></td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">6</td></tr>
-<tr class="dead"><td class="name"><a href="https://cbro.berotato.org/morgue//RoGGa/morgue-RoGGa-20260511-184724.txt">RoGGa</a></td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">4</td></tr>
-<tr class="dead"><td class="name"><a href="https://data.crawlcosplay.org/morgue//RoGGaTest2/morgue-RoGGaTest2-20260511-160054.txt">RoGGaTest2</a></td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
-<tr class="dead"><td class="name"><a href="https://crawl.roguelikes.gg/morgue//RoGGaTestCRG/morgue-RoGGaTestCRG-20260511-203253.txt">RoGGaTestCRG</a></td><td class="pt">0</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
-<tr class="none"><td class="name">Jannitor</td>
+    </tr><tr class="dead"><td class="name"><a href="http://crawl.xtahua.com/crawl/morgue/THILQ/morgue-THILQ-20260511-140308.txt">THILQ</a></td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">5</td></tr>
+<tr class="none"><td class="name">RoGGaTest</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCRG</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Jannitor</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Salticid</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">baljet</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCUE</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGa</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">agentchuck</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">TheMeInTeam</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Astinus</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Galadan</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Platt</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Myro</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTest2</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">rago</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Kergan</td>
-                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">THILQ</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Colgate</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Blyxx</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Archion</td>
@@ -51,11 +49,13 @@
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Hungry0364</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">ElricMelnibone</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Caterwaul</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCBRG</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">krsz</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCCO</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCBRO</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Quoth</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCXC</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCAO</td>
                 <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGaTestCDI</td>
                 <td colspan="15"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><img src="/img/animated-arrow.gif" width="40" height="10">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu"><a href="5.html">Week 5</a> - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-13 12:28 UTC</span></div>
+    <div id="bottomtext"><span class="menu"><img src="/img/animated-arrow.gif" width="40" height="10">CCSDT #3 for v0.34</a> : </span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu"><a href="5.html">Week 5</a> - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-13 12:38 UTC</span></div>
