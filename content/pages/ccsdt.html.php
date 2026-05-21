71b08016464937f71e494b75395c6289546eb9c0
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-05-21 10:47:27 -0400

    Update ccsdt.html.php

--- content/pages/ccsdt.html.php
+++ content/pages/ccsdt.html.php
@@ -1,12 +1,13 @@
 <?php $this->layout = 'ccsdt'; ?>
 <h2 style="color:rgb(69, 136, 5);">Welcome to the <b>Crawl Cosplay Sudden Death Tournament</b> (CCSDT)</h2>
 
-<h2>Next Tournament <b>CCSDT#3 for DCSS v0.34</b></h2>
+<h2>Current Tournament <b>CCSDT#3 for DCSS v0.34</b> - <b>LIVE NOW!</b></h2>
 <ul>
     <li>Starts: Friday May 15th, 2026 at 00:00 UTC. (Estimated local date & time: <a id="local-time"></a>)</li>
     <li><a href="/ccsdt/0.34/index.html">Tournament Overview</a>.</li>
     <li><a href="/ccsdt/0.34/rules.html">Tournament Details</a>.</li>
-    <li>NOTE: Once you make your way to either of the pages linked above, use the nav bar at the bottom of the page to navigate to all of the available pages.</li>
+    <li>NOTE: Once you make your way to either of the pages linked above, use the navigation bar at the bottom of the page to navigate to all of the available pages.</li>
+    <li>There is no deadline to be included during the torunament; you can skip week 1 and still join.</li>
 </ul>
 
 <script>
