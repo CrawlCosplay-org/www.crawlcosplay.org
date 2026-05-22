b05555560a5df44fe0358b75bb3f9ad7c6ec318f
RoGGa <robert_gagne69@yahoo.com>
2026-05-21 20:00:06 -0400

    On Demand Update

---
+++ content/pages/ccsdt/0.34/2.html.php
@@ -0,0 +1,144 @@
+<title>Week 2 &mdash; Erica (OpEn)</title>
+    <?php $this->layout = 'ccsdt'; ?>
+    <meta http-equiv="refresh" content="240"><div id="title">
+    <h2 id="sdc"><center>CCSDT#3 for DCSS v0.34</center></h2>
+    <h2>Week 2 &mdash; Erica (OpEn)</h2></div><div id="content"><div id="times"><span class="label">Week of: </span>2026-05-22 (ends at start of 2026-05-29 00:00 UTC)</div><div id="combo"><span class="label">Character: </span>Octopode Enchanter</div>
+<div id="bonus"><span class="label">Bonus 1: </span>Reach the end of Lair at XL &leq; 12.<br/>
+<span class="label">Bonus 2: </span>Collect the golden rune.</div>
+<div id="gods"><span class="label">Gods: </span>Gozag, Jiyva, Hepliaklqana</div><br><pre>LEGEND<br>------<br>Green = Won<br>Red   = Died<br>Grey  = ongoing or did not finish<br>        before end of the week</right></pre><pre>SPECIAL NOTE<br>------------<br>None</pre><div class="card"><table><tr class="head">
+    <th>Player</th>
+    <th>Reach XL5</th>
+    <th>Kill a Unique</th>
+    <th>Worship a Valid God</th>
+    <th>Reach XL10</th>
+    <th>Branch Enter</th>
+    <th>Branch End</th>
+    <th>Champion a Valid God</th>
+    <th><abbr title="Does not need to stay intact">Collect 1 Gem</abbr></th>
+    <th>Collect 1 Rune</th>
+    <th>Collect 2 Runes</th>
+    <th>Collect 3 Runes</th>
+    <th>Obtain Orb of Zot</th>
+    <th>Win</th>
+    <th>Bonus #1</th>
+    <th>Bonus #2</th>
+    <th>Week's Total (max=15)</th>
+    </tr><tr class="none"><td class="name">RoGGaTestCCO</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGa</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Jaredude</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">justinlove</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">stinkchinker</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">N1ckwad</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">elguitar</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Pascal76</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">LordBelias</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">James</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">flt</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">achylon</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">TomBombadil</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">CrazyBamboolord</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">nojaa</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">kuniqs</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Hasart</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">goupaloupa</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Jannitor</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Migumi</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Salticid</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">predelnik</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">cron3</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">BadBadger</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">aimeelou</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">baljet</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">erikjake</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">agentchuck</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Midpoint</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">TheMeInTeam</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Nonny</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">PieDemon</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">community</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">quinnr</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Astinus</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">particleface</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Strat</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Larett</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Galadan</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Platt</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">croikle</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">attentat</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Myro</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Patrick2011b</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">mobilemanners</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">rago</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Kergan</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">pythonsnek</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">TCC</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">THILQ</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">ventricule</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">frus</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Arcadio</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">jt</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Shym</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">IjonTichy1</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Tungsten</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Fougere</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Colgate</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Voda</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">ThePixelVillain</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">eccles</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Nebesus</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Endy73</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">aj12037</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Wolffangen</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">emiel</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">InabaYap</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Damoeire</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Quoth</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">jaccarmac</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">AutarchDCSS</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Blyxx</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">SlowGraham</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Archion</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">petercordia</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">TooFastSpriggan</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">ASCIIPhilia</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">opking</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Tanach</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">sekai</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Mneme</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Magatonman</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Mumonspawn</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">De02</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">yakattack</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">brinewave</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">wagawaga</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Hungry0364</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">thelochok</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">ElricMelnibone</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Caterwaul</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">dizzi</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">krsz</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">tmoiynmwg</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">BlueFrogs</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">vayu</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">prygans</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">XavTav</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">haldagan</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Loefaen</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Barata</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Mene</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">xeno23</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">here</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">MisterOstroum</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">DraconiusTourney</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">vanbol</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">riffraff</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Leibowitz</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">drwombats</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">lontrex</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Th3BaraKing</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">D1NKY</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">Wizard1ke</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">vayu1</td>
+                <td colspan="15"></td><td class="total">0</td></tr><tr class="none"><td class="name">icedessert</td>
+                <td colspan="15"></td><td class="total">0</td></tr></table></div></div>
+    <div id="bottomtext"><span class="menu">CCSDT for v0.34</a> : </span><span class="menu"><img src="/img/animated-arrow.gif" width="40" height="10"></span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu">Week 2 - </span><span class="menu">Week 3 - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Update: </span>2026-05-22 00:00 UTC</span></div>
