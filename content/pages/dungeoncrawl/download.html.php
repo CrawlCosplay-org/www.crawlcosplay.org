8ab1707f63db549f321791854acc23cc12f30b3e
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-08 08:47:49 -0400

    Delete content/pages/dungeoncrawl directory

--- content/pages/dungeoncrawl/download.html.php
+++
@@ -1,7 +0,0 @@
-<?php $this->layout = 'dungeoncrawl'; ?>
-<ul>
-    <li><h4><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download the BCadren fork</a></h4></li>
-    <li><h4><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download the BCrawl fork</a></h4></li>
-    <li><h4><a href="https://github.com/damerell/crawl/releases" target="_blank">Download the Stoat Soup fork</a></h4></li>
-    <li><h4><a href="/dungeoncrawl/download_dcss">Download the Stone Soup (DCSS) fork</a></h4></li>
-</ul>
