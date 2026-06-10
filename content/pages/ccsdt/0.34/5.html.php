4324a5e1098cdceb99445a2f8249b489305582d3
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-10 00:37:53 -0400

    Update week label from 'Week 4' to 'Week 5'

--- content/pages/ccsdt/0.34/5.html.php
+++ content/pages/ccsdt/0.34/5.html.php
@@ -3,6 +3,6 @@
     <meta http-equiv="refresh" content="240">
 <div id="title">
     <h2 id="sdc"><center>CCSDT#3 for DCSS v0.34</center></h2>
-    <h2>Week 4 &mdash; To Be Determined</h2>
+    <h2>Week 5 &mdash; To Be Determined</h2>
 </div>
 <div id="content"><div id="times"><span class="label">Week of: </span>2026-06-12 00:00 UTC</div>
