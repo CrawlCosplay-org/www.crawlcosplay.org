84caaf1ccc7af56de4d883e76c3f8404b5227672
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-10 01:05:22 -0400

    Update Week 5 challenge details in 5.html.php
    Added details about the weekly challenge format and streaming.

--- content/pages/ccsdt/0.34/5.html.php
+++ content/pages/ccsdt/0.34/5.html.php
@@ -4,6 +4,10 @@
 <div id="title">
     <h2 id="sdc"><center>CCSDT#3 for DCSS v0.34</center></h2>
     <h2>Week 5 &mdash; To Be Determined</h2>
+    <p>Each weekly challenge consists of playing a randomly chosen Crawl Unqiue's starting combo (e.g. Snorg, a TrBe). 
+        The chosen combo for each week of the competition will be streamed on Twitch.tv by RoGGa ( www.twitch.tv/rogga_likes_dcss ) 15 minutes before the start of the new week. 
+        A spin-the-wheels format will be used to make the choices random.
+    </p>
 </div>
 <div id="content"><div id="times"><span class="label">Week of: </span>2026-06-12 00:00 UTC</div>
     <div id="bottomtext"><span class="menu">CCSDT for v0.34</a> : </span><span class="menu"><img src="/img/animated-arrow.gif" width="40" height="10"></span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu">Week 5</span><span id="updated"></div>
