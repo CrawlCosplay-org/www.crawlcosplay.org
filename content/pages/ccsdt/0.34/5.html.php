11fec75b5b1853ae42184882fd0564c82c6ccc3e
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-10 00:40:11 -0400

    Add bottom text section to Week 5 page

--- content/pages/ccsdt/0.34/5.html.php
+++ content/pages/ccsdt/0.34/5.html.php
@@ -6,3 +6,4 @@
     <h2>Week 5 &mdash; To Be Determined</h2>
 </div>
 <div id="content"><div id="times"><span class="label">Week of: </span>2026-06-12 00:00 UTC</div>
+    <div id="bottomtext"><span class="menu">CCSDT for v0.34</a> : </span><span class="menu"><img src="/img/animated-arrow.gif" width="40" height="10"></span><span class="menu"><a href="index.html">Overview</a> - </span><span class="menu"><a href="rules.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu">Week 5</span><span id="updated"></div>
