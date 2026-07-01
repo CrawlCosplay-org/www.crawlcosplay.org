ff3d5fb2bc60783f0682ddb17011c5419b0e031c
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-01 11:14:40 -0400

    Update header image in neck.html.php

--- content/layouts/dcf/neck.html.php
+++ content/layouts/dcf/neck.html.php
@@ -15,7 +15,7 @@
 	</div>    <div class="toplinks"><br></div>
     <div class="page_content">
         <div class="heading">
-            <h1><img src="/img/cco_stone_soup_icon-512x512.png" width="48" height="48" /> DCF.DungeonCrawlForks.org <img src="/img/cco_stone_soup_icon-512x512.png" width="48" height="48" /></h1>
+            <h1><img src="/img/dcf-caldron.png" width="48" height="48" /> DCF.DungeonCrawlForks.org <img src="/img/dcf-caldron.png" width="48" height="48" /></h1>
 	    <div class="fineprint"><center>Come chat with us in our <a href="https://discord.gg/pW7nqC8Wu3" target="_blank">Crawl Cosplay 2.0 community discord server<img src="/img/discord_transparent_border.png" width="18" height="18" ></a></center></div>
         </div>
 	<br>
