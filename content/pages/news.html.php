d3a9c9e7b481bd3c513e995fe87d8b448be05192
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-10 07:13:15 -0400

    Revise NEWS page with recent announcements
    Updated the NEWS page with the latest updates on Crawl Cosplay and Dungeon Crawl.

--- content/pages/news.html.php
+++ content/pages/news.html.php
@@ -1,1 +1,13 @@
-NEWS page
+<h2>The latest NEWS</h2>
+<h3>Crawl Cosplay related news</h3>
+<ul><li>During July, the leadership of Crawl Cosplay will be handed over progressively from RoGGa to Colgate, with Draconius offering support to Colgate for external PR things, especially for communications with the DCSS team. 
+        FYI, the primary purpose of this leadership transfer is to try to achieve the remaining 2026 ojectives for Crawl Cosplay in regards to interactions with the DCSS team.</li>
+    <li>The<a href="/ccft">Crawl Cosplay Forks Tournament</a> (CCFT#2) will start in August.</li>
+	  <li>The<a href="/cctt">Crawl Cosplay Trunk Tournament</a> (CCTT#4) will likely start in October.</li>
+</ul>
+
+<h3>General Dungeon Crawl news</h3>
+<ul><li>A new Dungeon Crawl fork was launched in June and it is named: <a href='https://www.dungeoncrawlforks.org/forks/chili' target='_blank'><b>Dungeon Crawl Chili</b></a> (DC Chili)</li>
+    <li>DC Chili is available to play online on the new Webtiles server <a href='https://dcf.dungeoncrawlforks.org/' target='_blank'><b>Dungeon Crawl Fajita (DCF)</b></a> located in Ontario Canada</li>
+    <li>During July, the webtiles server <a href='https://cco.crawlcosplay.org' target='_blank'><b>CCO</b></a> will be moving to a European server in order to reduce monthly costs and will also be hosting Dungeon Crawl Chili.</li>
+</ul>
