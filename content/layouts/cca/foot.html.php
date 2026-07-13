12d5cc3567a1097775a135bbd1e42fc8cfe1129a
Draconius <Draconius0013@gmail.com>
2026-07-13 17:04:25 +1200

    Fix footer HTML structure for bug report link

--- content/layouts/cca/foot.html.php
+++ content/layouts/cca/foot.html.php
@@ -1,7 +1,8 @@
 		<br />
 		<img src="/img/HR-right.png" style="width:100%; height:auto;><br />
 		<br />
-		<footer>Copyleft  |  <a href="https://github.com/CrawlCosplay-org/www.crawlcosplay.org/issues" target="_blank">Send bug report or feature request</a>
+		<footer>Copyleft
+		<a href="https://github.com/CrawlCosplay-org/www.crawlcosplay.org/issues" target="_blank">Send bug report or feature request</a>
 		</footer>
 		</div>
 	</div>
