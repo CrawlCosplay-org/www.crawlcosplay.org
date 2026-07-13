3b0446b655e34760cea13451ff58f1895910236d
Draconius <Draconius0013@gmail.com>
2026-07-13 17:12:43 +1200

    Mobile Friendly fix

--- content/layouts/cca/foot.html.php
+++ content/layouts/cca/foot.html.php
@@ -1,5 +1,5 @@
 		<br />
-		<img src="/img/HR-right.png"><br />
+		<img src="/img/HR-right.png" style="width:100%; height:auto;><br />
 		<br />
 		<footer>Copyleft  |  <a href="https://github.com/CrawlCosplay-org/www.crawlcosplay.org/issues" target="_blank">Send bug report or feature request</a></footer>
 		</div>
