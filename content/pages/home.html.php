e0342c6f5fa34dcf2da2a565ec4b92493f7f6b8b
Draconius <Draconius0013@gmail.com>
2026-07-13 17:27:06 +1200

    Spacing Fix

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -68,9 +68,9 @@ echo	'<img src="/img/cc_stone_soup_icon-512x512.png"  width="350"  style="float:
 	echo "<p>"  . _("See the") . ' <a href="/news"><b>' . _("Dedicated Webpage") . "</b></a> "  
 		    . _("for the latest updates.") . "</p><br>";
 
-	echo "<h3 style='color:orange'>" . _("On behalf of the Crawl Cosplay community, thank you, Rogga, for the years of dedication to Crawl Cosplay") . "!</h3>";
+	echo "<h3 style='color:orange'>" . _("On behalf of the Crawl Cosplay community, thank you, Rogga, for the years of dedication to Crawl Cosplay") . "!</h3>"<br>;
 ?>
-<br>
+
 <br>
 <h3><b><center>Unique Visitors since June 2026</center></b></h3>
 <p><center><script type="text/javascript" src="https://counter.websiteout.com/js/2/5/-1/1"></script></center></p>
