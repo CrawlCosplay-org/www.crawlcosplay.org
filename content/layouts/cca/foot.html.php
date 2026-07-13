9b5a9dfa395368b967833895bba28bb3c6044338
Draconius <Draconius0013@gmail.com>
2026-07-13 17:11:02 +1200

    Update footer

--- content/layouts/cca/foot.html.php
+++ content/layouts/cca/foot.html.php
@@ -1,9 +1,7 @@
 		<br />
-		<img src="/img/HR-right.png" style="width:100%; height:auto;><br />
+		<img src="/img/HR-right.png"><br />
 		<br />
-		<footer>Copyleft
-		<a href="https://github.com/CrawlCosplay-org/www.crawlcosplay.org/issues" target="_blank">Send bug report or feature request</a>
-		</footer>
+		<footer>Copyleft  |  <a href="https://github.com/CrawlCosplay-org/www.crawlcosplay.org/issues" target="_blank">Send bug report or feature request</a></footer>
 		</div>
 	</div>
 
