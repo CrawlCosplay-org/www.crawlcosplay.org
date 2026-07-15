8d471cbe82f84bdcb2d62038985493cf4554990f
Draconius <Draconius0013@gmail.com>
2026-07-15 14:51:44 +1200

    Remove Floating Ad

--- content/parts/GloamVaultAd.html.php
+++ content/parts/GloamVaultAd.html.php
@@ -7,14 +7,14 @@
 </div>
 
 <!-- Floating Banner Container -->
-<div class="floating-banner">
+<!--<div class="floating-banner">
 	<h4>Website Sponsor</h4>
 	<a href="https://store.steampowered.com/app/3460840/Gloamvault/?utm_source=dcss_site&utm_medium=referral&utm_campaign=dcss_site" target="_blank">
         <img src="/img/ads/gloamvault-rightbanner4.gif" alt="GloatVault image" title="This site is partially funded by the game GloamVault. Please visit their entry on Steam by clicking the image abouve to get them to increase their funding for this website.">
     </a>
 	<h4>a game with DCSS sprites</h4>
     <!-- Optional Close Button -->
-    <button onclick="this.parentElement.style.display='none'" class="close-btn">&times;</button>
+  <!--  <button onclick="this.parentElement.style.display='none'" class="close-btn">&times;</button>
 </div>
 
 <style>
