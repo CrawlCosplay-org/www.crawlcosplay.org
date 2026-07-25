ac99a268115e65495c44d2b72cd219aa4ac61d09
Draconius <Draconius0013@gmail.com>
2026-07-26 10:48:57 +1200

    Update Homepage CCFT

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -36,7 +36,7 @@ echo	'<img src="/img/cc_stone_soup_icon-512x512.png"  width="350"  style="float:
 	echo "<ul><li>" . _("A tournament lasting 5 weeks with each week highlighting a different Dungeon Crawl fork.") . "</li>";
 	echo     "<li>" . _("Read ") . ' <a href="/ccft/about_ccft">' . _("About CCFT") . "</a>.</li>";
 	echo     "<li>" . _("CCFT#2 will start on July 31.") . ' <img src="/img/misc/arrow-flashing-left.gif" width="40" height="10">' . "</li>";
-	echo     "<li>" . _("Read about the current and upcoming")  . ': <a href="https://www.dungeoncrawlforks.org/" target="_blank">' . _("Dungeon Crawl Forks") . "</a>.</li></ul><br>";
+	echo     "<li>" . _("For information about the 5 Dungeon Crawl forks taking part in CCFT#2, see")  . ': <a href="https://www.dungeoncrawlforks.org/" target="_blank">' . _("Dungeon Crawl Forks") . "</a>.</li></ul><br>";
 
 	echo '<h3><a href="/cctt">' . _("Crawl Cosplay Trunk Tournament") . "</a> (CCTT)</h3>";
    	echo "<ul><li>" . _("A DCSS tournament lasting about a month with each week highlighting some of the latest Trunk changes.") . "</li>";
