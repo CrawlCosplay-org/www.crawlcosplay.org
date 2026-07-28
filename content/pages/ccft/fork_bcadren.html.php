04f5211c45e1945be6486ed9a90db0221388487f
Draconius <Draconius0013@gmail.com>
2026-07-28 16:55:36 +1200

    Add CPO

--- content/pages/ccft/fork_bcadren.html.php
+++ content/pages/ccft/fork_bcadren.html.php
@@ -10,6 +10,7 @@
 <ul><li><a href="https://crawl.nemelex.cards/" target="_blank">CNC in South Korea</a></li>
     <li><a href="http://cco.crawlcosplay.org/" target="_blank">CCO Toronto</a></li>
     <li><a href="https://crawl.yiuf.org/" target="_blank">CYO in Seattle</a></li>
+    <li><a href="https://crawl.project357.org/" target="_blank">CPO in Australia</a></li>
 </ul>
 
 <p>Join the <a href="https://discord.gg/JyRcec3WaS" target="_blank">bcadren discord server<img src="/img/discord_transparent_border.png" width="18" height="18"></a></p>
