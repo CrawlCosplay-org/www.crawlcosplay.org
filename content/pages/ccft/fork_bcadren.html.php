d89f4533da4e4560c0faf67d5cc9ef8ce15ed1ea
Draconius <Draconius0013@gmail.com>
2026-07-28 23:27:23 +1200

    CYO Removed

--- content/pages/ccft/fork_bcadren.html.php
+++ content/pages/ccft/fork_bcadren.html.php
@@ -9,7 +9,6 @@
 <h4>BCadren Webtiles servers:</h4>
 <ul><li><a href="https://crawl.nemelex.cards/" target="_blank">CNC in South Korea</a></li>
     <li><a href="http://cco.crawlcosplay.org/" target="_blank">CCO Toronto</a></li>
-    <li><a href="https://crawl.yiuf.org/" target="_blank">CYO in Seattle</a></li>
     <li><a href="https://crawl.project357.org/" target="_blank">CPO in Australia</a></li>
 </ul>
 
