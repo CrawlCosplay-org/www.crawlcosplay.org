04734f64253ceb4db86940f85c74c445e5450046
Draconius <Draconius0013@gmail.com>
2026-07-30 10:40:21 +1200

    Shrink FT Video

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -44,7 +44,7 @@ echo '
         <iframe
             src="https://www.youtube.com/embed/awknvDg5gGM"
             title="Crawl Cosplay Forks Tournament"
-            style="position:absolute;top:0;left:0;width:100%;height:100%;border:0;"
+            style="position:absolute;top:0;left:0;width:80%;height:80%;border:0;"
             allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen>
         </iframe>
