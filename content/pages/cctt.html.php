ee9bc1f047adbcce465478c6f26468f315390d7e
Draconius <Draconius0013@gmail.com>
2026-09-05 19:30:19 +1200

    Fix Image cycler CCTT

--- content/pages/cctt.html.php
+++ content/pages/cctt.html.php
@@ -109,14 +109,18 @@ if ($active) :
 
 
 <?php else : // no active challenge
-	$images = glob($_SERVER['DOCUMENT_ROOT'] . '/img/titles/*.{jpg,jpeg,png,gif,webp}', GLOB_BRACE);
-	$image = $images[array_rand($images)];
-	$image = str_replace($_SERVER['DOCUMENT_ROOT'], '', $image);
+	// echo $this->part('pause');
+
+	$images = glob(__DIR__ . '/../../webroot/img/titles/*.{jpg,jpeg,png,gif,webp}', GLOB_BRACE);
+
+	if ($images) {
+		$image = str_replace(__DIR__ . '/../../webroot', '', $images[array_rand($images)]);
+		echo '<img src="'.$e($image).'" width="350" style="float:right">';
+	}
 
 	echo "<h2>There is currently no active Crawl Cosplay Trunk Tournament (CCTT)</h2>";
 	echo "The next CCTT for trunk v0.35 will likely start in September 2026!<br><br>";
 	echo 'For previous tournament results, see:';
-	echo '<img src="'.$e($image).'" width="350" style="float:right">';
 	echo '<ul><li><a href="/cctt/tresults.html?set=32">CCTT#1</a> June 2024</li>';
 	echo '<li><a href="/cctt/tresults.html?set=33">CCTT#2</a> March 2025</li>';
 	echo '<li><a href="/cctt/tresults.html?set=34">CCTT#3</a> December 2025</li>';
