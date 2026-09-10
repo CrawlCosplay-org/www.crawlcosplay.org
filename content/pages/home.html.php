85d8873bc7d5c9607f16bc1728d6f13ac08da135
Draconius <Draconius0013@gmail.com>
2026-09-11 10:16:01 +1200

    Add arrow to TT Home page

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -48,7 +48,7 @@ echo '
 */
 /* <img src="/img/misc/arrow-flashing-left.gif" width="40" height="10"> */
 	echo "<h2>" . _("Look Forward to Future Tournaments!") . "</h2>";
-	echo '<h3>(Starting Soon!) <a href="/cctt">' . _("Crawl Cosplay Trunk Tournament") . "</a> (CCTT)</h3>";
+	echo '<h3>(Starting Soon!) <a href="/cctt">' . _("Crawl Cosplay Trunk Tournament") . "</a> (CCTT) &nbsp;<img src=\"/img/misc/arrow-flashing-left.gif\" width=\"40\" height=\"10\"></h3>";
    	echo "<ul><li>" . _("A DCSS tournament lasting about a month with each week highlighting some of the latest Trunk changes.") . "</li>";
    	echo     "<li>" . _("Read") . ': <a href="/cctt/about_cctt">' . _("About CCTT") . "</a>.</li>";
    	echo 	 '<li><a href="/cctt/past_cctt_results">' . _("Past CCTT Results") . "</a></li></ul><br>";
