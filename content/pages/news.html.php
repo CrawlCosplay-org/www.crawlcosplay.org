63822172476f9d57cd330ad4325ef6010988727b
Draconius <Draconius0013@gmail.com>
2026-09-25 22:03:51 +1200

    Update news for CCTT and re:Wiki

--- content/pages/news.html.php
+++ content/pages/news.html.php
@@ -1,10 +1,11 @@
 <h2>The latest NEWS</h2>
 <h3>Crawl Cosplay-Related News</h3>
-<ul><li>The <a href="/cctt"><b>Crawl Cosplay Trunk Tournament</b></a> (CCTT#4) will start September 18th!</li>
+<ul><li>The <a href="/cctt"><b>Crawl Cosplay Trunk Tournament</b></a> (CCTT#4) has begun!</li>
 	<li>The <a href="/ccft"><b>Crawl Cosplay Forks Tournament</b></a> (CCFT#2) has finished - congratulations to Kuniqs on the win and to all the players who more than doubled participation over the first Forks Tournament!</li>
 </ul>
 
 <h3>General Dungeon Crawl News</h3>
-<ul><li>A new Dungeon Crawl fork was launched in June, named: <a href='https://www.dungeoncrawlforks.org/forks/chili' target='_blank'><b>Dungeon Crawl Chili</b></a> (DC Chili)</li>
-    <li>DC Chili is available to play online on the new Webtiles server <a href='https://dcf.dungeoncrawlforks.org/' target='_blank'><b>Dungeon Crawl Fajita (DCF)</b></a> located in Ontario Canada along with CPO in Australia and CXC in France.</li>
+<ul><li>The <a href='http://crawl.chaosforge.org/Crawl_Wiki' target='_blank'><b>DCSS Wiki</b></a> team is getting active again in the lead-up to 0.35! You can join the team and help keep this great source of information up to date for the community. Head to the How-To-Help page on the Wiki, or contact Draconius on Discord, to start!</li>
+	<li>A new Dungeon Crawl fork was launched in June, named: <a href='https://www.dungeoncrawlforks.org/forks/chili' target='_blank'><b>Dungeon Crawl Chili</b></a> (DC Chili)</li>
+    <li>DC Chili is available to play online on the new Webtiles server <a href='https://dcf.dungeoncrawlforks.org/' target='_blank'><b>Dungeon Crawl Fajita (DCF)</b></a> located in Ontario Canada, along with CPO in Australia and CXC in France.</li>
 </ul>
